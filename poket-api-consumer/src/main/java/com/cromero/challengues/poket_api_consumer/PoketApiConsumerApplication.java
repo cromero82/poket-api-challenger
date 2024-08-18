3904d9eb05c44179cbbc8c02d81a3f6b87654bc9
Carlos Romero <soportecarlosromero@gmail.com>
2024-08-18 10:56:21 -0500

    initializer default project

---
+++ poket-api-consumer/src/main/java/com/cromero/challengues/poket_api_consumer/PoketApiConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.cromero.challengues.poket_api_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoketApiConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoketApiConsumerApplication.class, args);
+	}
+
+}
